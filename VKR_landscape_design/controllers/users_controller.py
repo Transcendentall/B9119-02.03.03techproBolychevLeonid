24fda0d7b6d3c8da71f22c87a73f8dd2d8a237d5
Bolychev Leonid <transcendentalL@yandex.ru>
2023-04-23 21:03:36 +1000

    коммитик2

--- VKR_landscape_design/controllers/users_controller.py
+++ VKR_landscape_design/controllers/users_controller.py
@@ -2,16 +2,35 @@ import json
 from utils import get_db_connection
 from app import app
 from flask import request
-from models.users_model import get_users, delete_user
+from models.users_model import *
 
 @app.route('/api/users', methods=['GET'])
-def selectallusers():
+def select_all_users():
     conn = get_db_connection()
     x = get_users(conn)
     return json.dumps(x.to_dict(orient="records"))
 
+@app.route('/api/users', methods=['GET'])
+def select_all_users_without_password():
+    conn = get_db_connection()
+    x = get_users_without_password(conn)
+    return json.dumps(x.to_dict(orient="records"))
+
+@app.route('/api/users', methods=['GET'])
+def select_all_users_without_password_admins():
+    conn = get_db_connection()
+    x = get_users_without_password_admins(conn)
+    return json.dumps(x.to_dict(orient="records"))
+
+@app.route('/api/users', methods=['GET'])
+def select_all_users_without_password_noadmins():
+    conn = get_db_connection()
+    x = get_users_without_password_noadmins(conn)
+    return json.dumps(x.to_dict(orient="records"))
+
+
 @app.route('/api/users/delete', methods=['POST'])
-def deleteuser():
+def delete_user():
     conn = get_db_connection()
     x = delete_user(conn, request.get_json()['user_user_id'])
     return json.dumps({'message': "success"})
