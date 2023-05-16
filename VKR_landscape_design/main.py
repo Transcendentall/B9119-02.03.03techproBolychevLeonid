d6d3179970e1c3a55c5eda4fc79264772c1e4479
Bolychev Leonid <transcendentalL@yandex.ru>
2023-05-16 20:44:48 +1000

    чисто контроллер для юзеров

--- VKR_landscape_design/main.py
+++ VKR_landscape_design/main.py
@@ -1,57 +1,16 @@
-import sqlite3
-import numpy as np
-import pandas as pd
-from models.animals_model import *
-from models.grounds_model import *
-from models.plants_model import *
-from models.soils_model import *
-from models.territories_model import *
-from models.users_model import *
-from models.connection_territories_soils_model import *
-from models.connection_soils_grounds_model import *
-from models.connection_soils_plants_model import *
-from models.connection_plants_animals_model import *
-pd.options.display.max_rows = 100
-pd.options.display.max_columns = 100
+# main.py
 
-# PRAGMA FOREIGN_KEYS = on;
+from typing import Optional
+from controllers import UserController
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-con = sqlite3.connect("VKR.sqlite")
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
-con.executescript('''
+app = FastAPI()
 
- ''')
-
-
-
-con.commit()
-
-cursor = con.cursor()
-
-print(get_animals(con))
-print(get_users(con))
-print(get_grounds(con))
-print(get_plants(con))
-print(get_soils(con))
-print(get_territories(con))
-print(get_users_without_password(con))
-print(get_users_without_password_admins(con))
-print(get_users_without_password_noadmins(con))
-print()
-print('--------------------')
-print()
-
-#print(get_plants(con))
-#insert_plant(con, 'qqq', 'www', 'eee')
-#delete_plant(con, 6)
-#update_plant_name(con, 7, 'IVAN!')
-#print(get_plants(con))
-
-print(get_one_user(con, 1))
-print(get_one_user_without_password(con, 1))
-print(authorisation(con, 'lutiysidor', 'abracadabra123'))
-
-print(get_soils_grounds_plants_animals_for_territories(con, 43.03, 131.88))
-
-
-print()
+app.include_router(UserController.router)
